2fd815b97d54ef1aaffb73f749ac49254168b1de
Tobias <61boto1bif@hft-stuttgart.de>
2018-04-17 22:52:25 +0200

    #7 erste Umsetzung der Flugbahn das Geschoss fliegt auf einer Flugbahn und wird geworfen, wenn man irgendwo drückt

--- Battle of the Stereotypes/Battle of the Stereotypes/GameScene.swift
+++ Battle of the Stereotypes/Battle of the Stereotypes/GameScene.swift
@@ -21,6 +21,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var leftDummy: SKSpriteNode!
     var rightDummy: SKSpriteNode!
     
+    //Wurfgeschoss
+    var ball: SKSpriteNode!
+    
     var leftDummyHealthLabel:SKLabelNode!
     var leftDummyHealth:Int = 0 {
         didSet {
@@ -44,7 +47,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         leftDummy.physicsBody = SKPhysicsBody(rectangleOf: leftDummy.size)
         leftDummy.physicsBody?.isDynamic = true
-        
+        leftDummy.physicsBody?.affectedByGravity = false
         leftDummy.physicsBody?.categoryBitMask = dummyCategory
         leftDummy.physicsBody?.contactTestBitMask = weaponCategory
         leftDummy.physicsBody?.collisionBitMask = 0
@@ -55,6 +58,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         rightDummy.position = CGPoint(x: self.frame.size.width / 2 - 100, y: rightDummy.size.height / 2 - 250)
         
         rightDummy.physicsBody = SKPhysicsBody(rectangleOf: rightDummy.size)
+        rightDummy.physicsBody?.affectedByGravity = false
         rightDummy.physicsBody?.isDynamic = true
         
         rightDummy.physicsBody?.categoryBitMask = dummyCategory
@@ -80,8 +84,38 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         self.addChild(rightDummyHealthLabel)
         
-        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        //self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
         self.physicsWorld.contactDelegate = self
+        
+        //initialisiere das Wurfgeschoss
+        let ballTexture = SKTexture(imageNamed: "Krug")
+        ball = SKSpriteNode(texture: ballTexture)
+        ball.size = CGSize(width: 30, height: 30)
+        ball.position = CGPoint(x: self.frame.size.width / 2 - 600, y: leftDummy.size.height / 2 - 250)
+        ball.physicsBody = SKPhysicsBody(texture: ballTexture, size: ball.size)
+        ball.physicsBody?.mass = 1
+        ball.physicsBody?.allowsRotation=false
+        ball.physicsBody?.isDynamic=false
+        ball.physicsBody?.affectedByGravity=false
+        ball.physicsBody?.collisionBitMask=0x1 << 2
+        
+        self.addChild(ball)
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        //Schleife die schaut ob der Fire Button gedrückt wurde und entsprechend reagiert
+        //bis jetzt gibt es noch keinen Button und man kann überall klicken
+        for t in touches {
+            ball.physicsBody?.affectedByGravity=true
+            ball.physicsBody?.isDynamic=true
+            ball.physicsBody?.allowsRotation=true
+            ball.physicsBody?.applyImpulse(CGVector(dx: 600, dy: 660))
+            break
+        }
+        
+        
     }
     
     override func update(_ currentTime: TimeInterval) {
