4bfad3f2bd59d9db7fca3616947e9d5db3a3e1c3
Simongotnews <s.huebner94@gmx.de>
2018-04-30 14:34:29 +0200

    Anpassung der Bundeslandklassen, Verbesserte Integration in Kartenansicht (OOP-Ansatz)

--- Battle of the Stereotypes/Battle of the Stereotypes/Bundesland.swift
+++ Battle of the Stereotypes/Battle of the Stereotypes/Bundesland.swift
@@ -36,8 +36,82 @@ class Bundesland: SKSpriteNode {
         self.zPosition = 0
     }
     
-    func setColor(){
-        
+    func switchColorToBlue(){
+        switch self.blName.hashValue{
+            case 0:
+                super.texture = SKTexture(imageNamed: "BadenWuertemberg_blue")
+            case 1:
+                super.texture = SKTexture(imageNamed: "Bayern_blue")
+            case 2:
+                super.texture = SKTexture(imageNamed: "Berlin_blue")
+            case 3:
+                super.texture = SKTexture(imageNamed: "Brandenburg_blue")
+            case 4:
+                super.texture = SKTexture(imageNamed: "Bremen_blue")
+            case 5:
+                super.texture = SKTexture(imageNamed: "Hamburg_blue")
+            case 6:
+                super.texture = SKTexture(imageNamed: "Hessen_blue")
+            case 7:
+                super.texture = SKTexture(imageNamed: "MecklenburgVorpommern_blue")
+            case 8:
+                super.texture = SKTexture(imageNamed: "Niedersachsen_blue")
+            case 9:
+                super.texture = SKTexture(imageNamed: "NRW_blue")
+            case 10:
+                super.texture = SKTexture(imageNamed: "RheinlandPfalz_blue")
+            case 11:
+                super.texture = SKTexture(imageNamed: "Saarland_blue")
+            case 12:
+                super.texture = SKTexture(imageNamed: "Sachsen_blue")
+            case 13:
+                super.texture = SKTexture(imageNamed: "SachsenAnhalt_blue")
+            case 14:
+                super.texture = SKTexture(imageNamed: "SchlesswigHolstein_blue")
+            case 15:
+                super.texture = SKTexture(imageNamed: "Thueringen_blue")
+            default:
+                print("Bundesland nicht vorhanden!")
+        }
+    }
+    
+    func switchColorToRed(){
+        switch self.blName.hashValue{
+        case 0:
+            super.texture = SKTexture(imageNamed: "BadenWuertemberg_red")
+        case 1:
+            super.texture = SKTexture(imageNamed: "Bayern_red")
+        case 2:
+            super.texture = SKTexture(imageNamed: "Berlin_red")
+        case 3:
+            super.texture = SKTexture(imageNamed: "Brandenburg_red")
+        case 4:
+            super.texture = SKTexture(imageNamed: "Bremen_red")
+        case 5:
+            super.texture = SKTexture(imageNamed: "Hamburg_red")
+        case 6:
+            super.texture = SKTexture(imageNamed: "Hessen_red")
+        case 7:
+            super.texture = SKTexture(imageNamed: "MecklenburgVorpommern_red")
+        case 8:
+            super.texture = SKTexture(imageNamed: "Niedersachsen_red")
+        case 9:
+            super.texture = SKTexture(imageNamed: "NRW_red")
+        case 10:
+            super.texture = SKTexture(imageNamed: "RheinlandPfalz_red")
+        case 11:
+            super.texture = SKTexture(imageNamed: "Saarland_red")
+        case 12:
+            super.texture = SKTexture(imageNamed: "Sachsen_red")
+        case 13:
+            super.texture = SKTexture(imageNamed: "SachsenAnhalt_red")
+        case 14:
+            super.texture = SKTexture(imageNamed: "SchlesswigHolstein_red")
+        case 15:
+            super.texture = SKTexture(imageNamed: "Thueringen_red")
+        default:
+            print("Bundesland nicht vorhanden!")
+        }
     }
     
     func setPosition(){
--- Battle of the Stereotypes/Battle of the Stereotypes/GermanMap.swift
+++ Battle of the Stereotypes/Battle of the Stereotypes/GermanMap.swift
@@ -27,6 +27,24 @@ class GermanMap: SKScene {
     
     var mapSize:(width:CGFloat, height:CGFloat) = (0.0, 0.0)  // globale Groeße welche in allen Funktionen verwendet werden kann.
     
+    // Bundeslaender deklarieren:
+    var badenWuertemberg:Bundesland?
+    var bayern:Bundesland?
+    var berlin:Bundesland?
+    var brandenburg:Bundesland?
+    var bremen:Bundesland?
+    var hamburg:Bundesland?
+    var hessen:Bundesland?
+    var mecklenburgVorpommern:Bundesland?
+    var niedersachsen:Bundesland?
+    var nordrheinWestfalen:Bundesland?
+    var rheinlandPfalz:Bundesland?
+    var saarland:Bundesland?
+    var sachsen:Bundesland?
+    var sachsenAnhalt:Bundesland?
+    var schlesswigHolstein:Bundesland?
+    var thueringen:Bundesland?
+    
     override func didMove(to view: SKView) {
         //Setze den Schwerpunkt der gesamten Scene auf die untere linke Ecke
         self.anchorPoint = CGPoint(x: 0, y: 0)
@@ -44,19 +62,6 @@ class GermanMap: SKScene {
         if playButton.contains(touch.location(in: self)){
             transitToGameScene()
         }
-    }
-    
-    func setBGMap(){
-        // Setze die Hintergrundkarte (grau) auf Deutschlandkartenhaelfte:
-        let backgroundMap = SKSpriteNode(imageNamed: "Deutschlandkarte")
-        backgroundMap.xScale = 0.70                     // Skalieren auf passende Viewgröße
-        backgroundMap.yScale = 0.80
-        backgroundMap.position = CGPoint(x: 0, y: 0)    // Anker am Viewrand
-        backgroundMap.zPosition = 1
-        mapSide.addChild(backgroundMap)
-        
-        // die Size als globales Tupel speichern fuer BL
-        mapSize = (backgroundMap.size.width, backgroundMap.size.height)
     }
     
     func splitScene() {
@@ -82,165 +87,168 @@ class GermanMap: SKScene {
         rightScene.addChild(statsSide)
         
         setBGMap()
-        
-        // Hinzufuegen der einzelnen BL an der korrekten Stelle als Klasse Bundesland:
-        // HINWEIS: die groesse der einzelnen Kartenelemente richtet sich nach der Size der Hintergrundmap!
-        // BW:
-        var badenWuertemberg = Bundesland(blName: BundeslandEnum.BadenWuerttemberg, texture: SKTexture(imageNamed: "BadenWuertemberg_blue"),
-            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
-        badenWuertemberg.setPosition()
-        mapSide.addChild(badenWuertemberg)
-        
-        // Bayern:
-        var bayern = Bundesland(blName: BundeslandEnum.Bayern, texture: SKTexture(imageNamed: "Bayern_blue"),
-            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
-        bayern.setPosition()
-        mapSide.addChild(bayern)
-        
-        // Berlin:
-        var berlin = Bundesland(blName: BundeslandEnum.Berlin, texture: SKTexture(imageNamed: "Berlin_red"),
-            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
-        berlin.setPosition()
-        mapSide.addChild(berlin)
-        
-        // Brandenburg:
-        var brandenburg = Bundesland(blName: BundeslandEnum.Brandenburg, texture: SKTexture(imageNamed: "Brandenburg_red"),
-            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
-        brandenburg.setPosition()
-        mapSide.addChild(brandenburg)
-        
-        // Bremen:
-        var bremen = Bundesland(blName: BundeslandEnum.Bremen, texture: SKTexture(imageNamed: "Bremen_red"),
-            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
-        bremen.setPosition()
-        mapSide.addChild(bremen)
-        
-        // Hamburg:
-        var hamburg = Bundesland(blName: BundeslandEnum.Hamburg, texture: SKTexture(imageNamed: "Hamburg_red"),
-            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
-        hamburg.setPosition()
-        mapSide.addChild(hamburg)
-        
-        // Hessen:
-        var hessen = Bundesland(blName: BundeslandEnum.Hessen, texture: SKTexture(imageNamed: "Hessen_red"),
-            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
-        hessen.setPosition()
-        mapSide.addChild(hessen)
-        
-        // Mecklenburg-Vorpommern:
-        var mecklenburgVorpommern = Bundesland(blName: BundeslandEnum.MecklenburgVorpommern, texture: SKTexture(imageNamed: "MecklenburgVorpommern_red"),
-            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
-        mecklenburgVorpommern.setPosition()
-        mapSide.addChild(mecklenburgVorpommern)
-        
-        // Niedersachsen:
-        var niedersachsen = Bundesland(blName: BundeslandEnum.Niedersachsen, texture: SKTexture(imageNamed: "Niedersachsen_blue"),
-            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
-        niedersachsen.setPosition()
-        mapSide.addChild(niedersachsen)
-        
-        // Nordrhein-Westfalen:
-        var nordrheinWestfalen = Bundesland(blName: BundeslandEnum.NordrheinWestfalen, texture: SKTexture(imageNamed: "NRW_blue"),
-            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
-        nordrheinWestfalen.setPosition()
-        mapSide.addChild(nordrheinWestfalen)
-        
-        // Rheinland-Pfalz:
-        var rheinlandPfalz = Bundesland(blName: BundeslandEnum.RheinlandPhalz, texture: SKTexture(imageNamed: "RheinlandPfalz_red"),
-            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
-        rheinlandPfalz.setPosition()
-        mapSide.addChild(rheinlandPfalz)
-        
-        // Saarland:
-        var saarland = Bundesland(blName: BundeslandEnum.Saarland, texture: SKTexture(imageNamed: "Saarland_blue"),
-            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
-        saarland.setPosition()
-        mapSide.addChild(saarland)
-        
-        // Sachsen:
-        var sachsen = Bundesland(blName: BundeslandEnum.Sachsen, texture: SKTexture(imageNamed: "Sachsen_red"),
-            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
-        sachsen.setPosition()
-        mapSide.addChild(sachsen)
-        
-        // Sachsen-Anhalt:
-        var sachsenAnhalt = Bundesland(blName: BundeslandEnum.SachsenAnhalt, texture: SKTexture(imageNamed: "SachsenAnhalt_red"),
-            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
-        sachsenAnhalt.setPosition()
-        mapSide.addChild(sachsenAnhalt)
-        
-        // Schlesswig-Holstein:
-        var schlesswigHolstein = Bundesland(blName: BundeslandEnum.SchleswigHolstein, texture: SKTexture(imageNamed: "SchlesswigHolstein_red"),
-            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
-        schlesswigHolstein.setPosition()
-        mapSide.addChild(schlesswigHolstein)
-        
-        // Thueringen:
-        var thueringen = Bundesland(blName: BundeslandEnum.Bremen, texture: SKTexture(imageNamed: "Thueringen_red"),
-            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
-        thueringen.setPosition()
-        mapSide.addChild(thueringen)
+        initBundeslaender()
         
         // testweise BL in den Hintergrund schicken:
-        hessen.toBackground()
-        berlin.toBackground()
-        schlesswigHolstein.toBackground()
-        nordrheinWestfalen.toBackground()
+        //hessen?.toBackground()
+        //berlin?.toBackground()
+        //schlesswigHolstein?.toBackground()
+        //nordrheinWestfalen?.toBackground()
+        //allToRed()
+        //badenWuertemberg?.switchColorToBlue()
         
         self.addChild(leftScene)
         self.addChild(rightScene)
     }
     
-    func initBundeslaenderPNGs(){
-        let badenWuertemberg_red = SKSpriteNode(imageNamed: "BadenWuertemberg_red")    // Initialisieren der beiden Farben
-        let badenWuertemberg_blue = SKSpriteNode(imageNamed: "BadenWuertemberg_blue")
+    func setBGMap(){
+        // Setze die Hintergrundkarte (grau) auf Deutschlandkartenhaelfte:
+        let backgroundMap = SKSpriteNode(imageNamed: "Deutschlandkarte")
+        backgroundMap.xScale = 0.70                     // Skalieren auf passende Viewgröße
+        backgroundMap.yScale = 0.80
+        backgroundMap.position = CGPoint(x: 0, y: 0)    // Anker am Viewrand
+        backgroundMap.zPosition = 1
+        mapSide.addChild(backgroundMap)
         
-        let bayern_red = SKSpriteNode(imageNamed: "Bayern_red")
-        let bayern_blue = SKSpriteNode(imageNamed: "Bayern_blue")
-        
-        let berlin_red = SKSpriteNode(imageNamed: "Berlin_red")
-        let berlin_blue = SKSpriteNode(imageNamed: "Berlin_blue")
-        
-        let brandenburg_red = SKSpriteNode(imageNamed: "Brandenburg_red")
-        let brandenburg_blue = SKSpriteNode(imageNamed: "Brandenburg_blue")
-        
-        let bremen_red = SKSpriteNode(imageNamed: "Bremen_red")
-        let bremen_blue = SKSpriteNode(imageNamed: "Bremen_blue")
-        
-        let hamburg_red = SKSpriteNode(imageNamed: "Hamburg_red")
-        let hamburg_blue = SKSpriteNode(imageNamed: "Hamburg_blue")
-        
-        let hessen_red = SKSpriteNode(imageNamed: "Hessen_red")
-        let hessen_blue = SKSpriteNode(imageNamed: "Hessen_blue")
-        
-        let mecklenburgVorpommern_red = SKSpriteNode(imageNamed: "MecklenburgVorpommern_red")
-        let mecklenburgVorpommern_blue = SKSpriteNode(imageNamed: "MecklenburgVorpommern_blue")
-        
-        let niedersachsen_red = SKSpriteNode(imageNamed: "Niedersachsen_red")
-        let niedersachsen_blue = SKSpriteNode(imageNamed: "Niedersachsen_blue")
-        
-        let nrw_red = SKSpriteNode(imageNamed: "NRW_red")
-        let nrw_blue = SKSpriteNode(imageNamed: "NRW_blue")
-        
-        let rheinlandPfalz_red = SKSpriteNode(imageNamed: "RheinlandPfalz_red")
-        let rheinlandPfalz_blue = SKSpriteNode(imageNamed: "RheinlandPfalz_blue")
-        
-        let saarland_red = SKSpriteNode(imageNamed: "Saarland_red")
-        let saarland_blue = SKSpriteNode(imageNamed: "Saarland_blue")
-        
-        let sachsen_red = SKSpriteNode(imageNamed: "Sachen_red")
-        let sachsen_blue = SKSpriteNode(imageNamed: "Sachsen_blue")
-        
-        let sachsenAnhalt_red = SKSpriteNode(imageNamed: "SachsenAnhalt_red")
-        let sachsenAnhalt_blue = SKSpriteNode(imageNamed: "SachsenAnhalt_blue")
-        
-        let schlesswigHolstein_red = SKSpriteNode(imageNamed: "SchlesswigHolstein_red")
-        let schlesswigHolstein_blue = SKSpriteNode(imageNamed: "SchlesswigHolstein_blue")
-        
-        let thueringen_red = SKSpriteNode(imageNamed: "Thueringen_red")
-        let thueringen_blue = SKSpriteNode(imageNamed: "Thueringen_blue")
-        
+        // die Size als globales Tupel speichern fuer BL
+        mapSize = (backgroundMap.size.width, backgroundMap.size.height)
     }
+    
+    func initBundeslaender(){
+        // Hinzufuegen der einzelnen BL an der korrekten Stelle als Klasse Bundesland:
+        // HINWEIS: die groesse der einzelnen Kartenelemente richtet sich nach der Size der Hintergrundmap!
+        // BW:
+        badenWuertemberg = Bundesland(blName: BundeslandEnum.BadenWuerttemberg, texture: SKTexture(imageNamed: "BadenWuertemberg_blue"), size: CGSize(width: (mapSize.width), height: (mapSize.height)))
+        badenWuertemberg?.setPosition()
+        mapSide.addChild(badenWuertemberg!)
+        
+        // Bayern:
+        bayern = Bundesland(blName: BundeslandEnum.Bayern, texture: SKTexture(imageNamed: "Bayern_blue"),
+            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
+        bayern?.setPosition()
+        mapSide.addChild(bayern!)
+        
+        // Berlin:
+        berlin = Bundesland(blName: BundeslandEnum.Berlin, texture: SKTexture(imageNamed: "Berlin_red"),
+            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
+        berlin?.setPosition()
+        mapSide.addChild(berlin!)
+        
+        // Brandenburg:
+        brandenburg = Bundesland(blName: BundeslandEnum.Brandenburg, texture: SKTexture(imageNamed: "Brandenburg_red"),
+            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
+        brandenburg?.setPosition()
+        mapSide.addChild(brandenburg!)
+        
+        // Bremen:
+        bremen = Bundesland(blName: BundeslandEnum.Bremen, texture: SKTexture(imageNamed: "Bremen_red"),
+            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
+        bremen?.setPosition()
+        mapSide.addChild(bremen!)
+        
+        // Hamburg:
+        hamburg = Bundesland(blName: BundeslandEnum.Hamburg, texture: SKTexture(imageNamed: "Hamburg_red"),
+            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
+        hamburg?.setPosition()
+        mapSide.addChild(hamburg!)
+        
+        // Hessen:
+        hessen = Bundesland(blName: BundeslandEnum.Hessen, texture: SKTexture(imageNamed: "Hessen_red"),
+            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
+        hessen?.setPosition()
+        mapSide.addChild(hessen!)
+        
+        // Mecklenburg-Vorpommern:
+        mecklenburgVorpommern = Bundesland(blName: BundeslandEnum.MecklenburgVorpommern, texture: SKTexture(imageNamed: "MecklenburgVorpommern_red"), size: CGSize(width: (mapSize.width), height: (mapSize.height)))
+        mecklenburgVorpommern?.setPosition()
+        mapSide.addChild(mecklenburgVorpommern!)
+        
+        // Niedersachsen:
+        niedersachsen = Bundesland(blName: BundeslandEnum.Niedersachsen, texture: SKTexture(imageNamed: "Niedersachsen_blue"),
+            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
+        niedersachsen?.setPosition()
+        mapSide.addChild(niedersachsen!)
+        
+        // Nordrhein-Westfalen:
+        nordrheinWestfalen = Bundesland(blName: BundeslandEnum.NordrheinWestfalen, texture: SKTexture(imageNamed: "NRW_blue"),
+            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
+        nordrheinWestfalen?.setPosition()
+        mapSide.addChild(nordrheinWestfalen!)
+        
+        // Rheinland-Pfalz:
+        rheinlandPfalz = Bundesland(blName: BundeslandEnum.RheinlandPhalz, texture: SKTexture(imageNamed: "RheinlandPfalz_red"),
+            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
+        rheinlandPfalz?.setPosition()
+        mapSide.addChild(rheinlandPfalz!)
+        
+        // Saarland:
+        saarland = Bundesland(blName: BundeslandEnum.Saarland, texture: SKTexture(imageNamed: "Saarland_blue"),
+            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
+        saarland?.setPosition()
+        mapSide.addChild(saarland!)
+        
+        // Sachsen:
+        sachsen = Bundesland(blName: BundeslandEnum.Sachsen, texture: SKTexture(imageNamed: "Sachsen_red"),
+            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
+        sachsen?.setPosition()
+        mapSide.addChild(sachsen!)
+        
+        // Sachsen-Anhalt:
+        sachsenAnhalt = Bundesland(blName: BundeslandEnum.SachsenAnhalt, texture: SKTexture(imageNamed: "SachsenAnhalt_red"),
+            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
+        sachsenAnhalt?.setPosition()
+        mapSide.addChild(sachsenAnhalt!)
+        
+        // Schlesswig-Holstein:
+        schlesswigHolstein = Bundesland(blName: BundeslandEnum.SchleswigHolstein, texture: SKTexture(imageNamed: "SchlesswigHolstein_red"), size: CGSize(width: (mapSize.width), height: (mapSize.height)))
+        schlesswigHolstein?.setPosition()
+        mapSide.addChild(schlesswigHolstein!)
+        
+        // Thueringen:
+        thueringen = Bundesland(blName: BundeslandEnum.Bremen, texture: SKTexture(imageNamed: "Thueringen_red"),
+            size: CGSize(width: (mapSize.width), height: (mapSize.height)))
+        thueringen?.setPosition()
+        mapSide.addChild(thueringen!)
+    }
+    
+    func allToRed(){
+        self.badenWuertemberg?.switchColorToRed()
+        self.bayern?.switchColorToRed()
+        self.berlin?.switchColorToRed()
+        self.brandenburg?.switchColorToRed()
+        self.bremen?.switchColorToRed()
+        self.hamburg?.switchColorToRed()
+        self.hessen?.switchColorToRed()
+        self.mecklenburgVorpommern?.switchColorToRed()
+        self.niedersachsen?.switchColorToRed()
+        self.nordrheinWestfalen?.switchColorToRed()
+        self.rheinlandPfalz?.switchColorToRed()
+        self.saarland?.switchColorToRed()
+        self.sachsen?.switchColorToRed()
+        self.sachsenAnhalt?.switchColorToRed()
+        self.schlesswigHolstein?.switchColorToRed()
+        self.thueringen?.switchColorToRed()
+    }
+   
+    func allToBlue(){
+        self.badenWuertemberg?.switchColorToBlue()
+        self.bayern?.switchColorToBlue()
+        self.berlin?.switchColorToBlue()
+        self.brandenburg?.switchColorToBlue()
+        self.bremen?.switchColorToBlue()
+        self.hamburg?.switchColorToBlue()
+        self.hessen?.switchColorToBlue()
+        self.mecklenburgVorpommern?.switchColorToBlue()
+        self.niedersachsen?.switchColorToBlue()
+        self.nordrheinWestfalen?.switchColorToBlue()
+        self.rheinlandPfalz?.switchColorToBlue()
+        self.saarland?.switchColorToBlue()
+        self.sachsen?.switchColorToBlue()
+        self.sachsenAnhalt?.switchColorToBlue()
+        self.schlesswigHolstein?.switchColorToBlue()
+        self.thueringen?.switchColorToBlue()
+    }
+    
     
     func initPlayButton() {
         playButton = SKSpriteNode(imageNamed: "play_Button")
