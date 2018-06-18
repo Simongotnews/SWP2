b9826ddaf52a9a68bd123444fe7307f86359a9f9
Tetiana Prokhorova <52prte1bif@hft-stuttgart.de>
2018-06-18 02:04:31 +0200

    Entfernen des Labels, das die Kraft des Geschosses anzeigt

--- Battle of the Stereotypes/Battle of the Stereotypes/GameScene.swift
+++ Battle of the Stereotypes/Battle of the Stereotypes/GameScene.swift
@@ -70,7 +70,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var forceCounter: Int = 0
     let powerBarGray = SKShapeNode(rectOf: CGSize(width: 200, height: 25))
     var powerBarGreen = SKShapeNode(rectOf: CGSize(width: 2, height: 25))
-    var powerLabel = SKLabelNode(fontNamed: "ArialMT")
+    
     
     //Hintergrund
     var background: SKSpriteNode!
@@ -516,13 +516,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         powerBarGreen.zPosition = 3
         powerBarGreen.xScale = CGFloat(0)
         self.addChild(powerBarGreen)
-        
-        powerLabel.fontColor = SKColor.darkGray
-        powerLabel.fontSize = 20
-        powerLabel.position.x = powerBarGray.position.x
-        powerLabel.position.y = powerBarGray.position.y + 30
-        powerLabel.zPosition = 3
-        self.addChild(powerLabel)
     }
     
     func initHealthBar(){ //initalisiere eine Bar zur Anzeige der verbleibenden Lebenspunkte des jeweiligen Dummys
@@ -612,7 +605,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             [unowned self] in
             if self.forceCounter < 100 {
                 self.forceCounter += 1
-                self.powerLabel.text = "\(self.forceCounter) %"
                 self.powerBarGreen.xScale = CGFloat(self.forceCounter)
                 self.powerBarGreen.position = CGPoint(x: 0 - CGFloat((100 - self.forceCounter)), y: 230)
             }else {
@@ -626,7 +618,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     func powerBarReset(){
         forceCounter = 0
-        powerLabel.removeFromParent()
         powerBarGray.removeFromParent()
         powerBarGreen.removeFromParent()
     }
